dccc70699a449782025acef458b77c29e35f8303
rz.oktan <rz.oktan@gmail.com>
2024-02-28 15:30:11 +0700

    update

--- components/HomeComponents/Banner.jsx
+++ components/HomeComponents/Banner.jsx
@@ -1,7 +1,7 @@
-import Typewriter from 'typewriter-effect';
-import BannerLayout from '../Common/BannerLayout';
-import { Link } from 'react-scroll';
-import Image from 'next/image';
+import Typewriter from "typewriter-effect"
+import BannerLayout from "../Common/BannerLayout"
+import { Link } from "react-scroll"
+import Image from "next/image"
 
 const Banner = () => {
     return (
@@ -11,37 +11,61 @@ const Banner = () => {
                     <div className="flex items-center md:items-center md:justify-around">
                         <div className="">
                             <div className="">
-                                <h1 className="text-3xl sm:text-4xl xl:text-5xl text-Snow font-bold">Hello, Check This Out!</h1>
+                                <h1 className="text-3xl sm:text-4xl xl:text-5xl text-Snow font-bold">
+                                    Hello, Check This Out!
+                                </h1>
                             </div>
                             <div className="">
                                 <div className=" py-4 font-cascadia-normal text-Snow pb-4 text-xs h-20 lg:h-auto">
-                                    <span>{"<"}<span className="text-[#a3ff04] sm:text-base xl:text-lg font-bold">div</span>
-                                        {">"} <span className="text-Snow sm:text-xl xl:text-2xl font-bold"> I am a  <span className="inline-block">
-                                            <Typewriter
-                                                options={{
-                                                    strings:
-                                                        [
-                                                            'Blockchain Developer',
-                                                            'MERN Stack Developer',
-                                                            'Web3 DApp Developer'
-                                                        ],
-                                                    autoStart: true,
-                                                    loop: true,
-                                                }}
-                                            />
+                                    <span>
+                                        {"<"}
+                                        <span className="text-[#a3ff04] sm:text-base xl:text-lg font-bold">
+                                            div
                                         </span>
-                                        </span> {"</"}<span className="text-[#a3ff04] sm:text-base xl:text-lg font-bold">div</span>{">"} </span>
+                                        {">"}{" "}
+                                        <span className="text-Snow sm:text-xl xl:text-2xl font-bold">
+                                            {" "}
+                                            I am a{" "}
+                                            <span className="inline-block">
+                                                <Typewriter
+                                                    options={{
+                                                        strings: [
+                                                            "Blockchain Developer",
+                                                            "MERN Stack Developer",
+                                                            "Web3 DApp Developer",
+                                                        ],
+                                                        autoStart: true,
+                                                        loop: true,
+                                                    }}
+                                                />
+                                            </span>
+                                        </span>{" "}
+                                        {"</"}
+                                        <span className="text-[#a3ff04] sm:text-base xl:text-lg font-bold">
+                                            div
+                                        </span>
+                                        {">"}{" "}
+                                    </span>
                                 </div>
                             </div>
-                            <Link to='intro' spy={true} smooth={true} duration={500} offset={-50} className="button">Explore</Link>
+                            <Link
+                                // to="intro"
+                                // spy={true}
+                                // smooth={true}
+                                // duration={100}
+                                // offset={-100}
+                                className="button"
+                            >
+                                Explore
+                            </Link>
                         </div>
                         <div className="w-48 h-52 relative hidden md:block">
                             <Image
-                            className='absolute top-8 w-full h-full'
-                            src="/images/main.png" 
-                            alt="emoji" 
-                            width={300}
-                            height={300}
+                                className="absolute top-8 w-full h-full"
+                                src="/images/main.png"
+                                alt="emoji"
+                                width={300}
+                                height={300}
                             />
                         </div>
                     </div>
@@ -49,27 +73,25 @@ const Banner = () => {
 
                 {/* details in row */}
                 <div className="grid grid-cols-2 gap-4 md:gap-0 md:flex items-center justify-between w-full px-4 xl:px-8 2xl:px-16">
-
                     <div className="flex items-center gap-x-1">
-                        <span className='text-base md:text-lg text-Green font-bold'>20+</span>
-                        <span className='text-xs text-Snow'>Completed Projects</span>
+                        <span className="text-base md:text-lg text-Green font-bold">20+</span>
+                        <span className="text-xs text-Snow">Completed Projects</span>
                     </div>
 
                     <div className="flex items-center gap-x-1">
-                        <span className='text-base md:text-lg text-Green font-bold'>10+</span>
-                        <span className='text-xs text-Snow'>Freelance Clients</span>
+                        <span className="text-base md:text-lg text-Green font-bold">10+</span>
+                        <span className="text-xs text-Snow">Freelance Clients</span>
                     </div>
 
                     <div className="flex items-center gap-x-1">
-                        <span className='text-base md:text-lg text-Green font-bold'>3+</span>
-                        <span className='text-xs text-Snow'>Honors & Awards</span>
+                        <span className="text-base md:text-lg text-Green font-bold">3+</span>
+                        <span className="text-xs text-Snow">Honors & Awards</span>
                     </div>
 
                     <div className="flex items-center gap-x-1">
-                        <span className='text-base md:text-lg text-Green font-bold'>25+</span>
-                        <span className='text-xs text-Snow'>Opensource Projects</span>
+                        <span className="text-base md:text-lg text-Green font-bold">25+</span>
+                        <span className="text-xs text-Snow">Opensource Projects</span>
                     </div>
-
                 </div>
             </div>
         </BannerLayout>
